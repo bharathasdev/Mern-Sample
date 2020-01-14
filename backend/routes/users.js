dcb120c3e9bf3d5fdc176ebc89f52489b987d664
Bharath Kumar <bkumar@innov8gaming.com>
2020-01-14 17:40:18 +0000

    First commit

---
+++ backend/routes/users.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const user = require('../models/user.model');
+
+router.route('/').get((req, res) =>{
+   
+    user.find()
+        .then( users => res.json(users))
+        .catch( err => res.status(400).json('Error:'+ err));
+
+});
+
+router.route('/add').post((req, res) =>{
+    const username = req.body.username;
+    const newUser = new user({username});
+    newUser.save()
+        .then( () => res.json('User Added!'))
+        .catch( err => res.status(400).json('Error:'+ err));
+
+});
+
+module.exports = router;
