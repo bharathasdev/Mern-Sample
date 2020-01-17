52cc1ed9a3ec6886ce249da366076be27d7a03ef
Bharath Kumar <bkumar@innov8gaming.com>
2020-01-17 16:03:23 +0000

    Built the front end and connected to the backend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import "bootstrap/dist/css/bootstrap.min.css"
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component.js";
+import EditExercise from "./components/edit-exercise.component.js";
+import CreateExercise from "./components/create-exercise.component.js";
+import CreateUser from "./components/create-user.component";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="container">
-      Hello World!
-    </div>
+    <Router>
+      <div className="container">
+        <Navbar />
+        
+        <br/>
+        <Route path = "/" exact component = {ExercisesList} />
+        <Route path = "/edit/:id" component = {EditExercise} />
+        <Route path = "/create" component = {CreateExercise} />
+        <Route path = "/user" component = {CreateUser} />
+     
+      </div>
+    </Router>
   );
 }
 
